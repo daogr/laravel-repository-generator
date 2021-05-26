d39f7b2ddda4568bd638b0d751f737b817721cab
daogr <dao@ddais.com>
2021-05-27 00:47:22 +0300

    Initial commit

---
+++ src/Utils/ResponseUtil.php
@@ -0,0 +1,45 @@
+<?php
+
+	namespace Otodev\Utils;
+
+	/**
+	 * Class ResponseUtil
+	 * @package Otodev\Utils
+	 */
+	class ResponseUtil {
+		const RESPONSE = ['success', 'data', 'code', 'message'];
+		const SUCCESS  = ['success', 'data', 'code', 'message'];
+		const ERROR    = ['success', 'code', 'message'];
+
+		/**
+		 * @param     $message
+		 * @param     $data
+		 * @param int $code
+		 *
+		 * @return array
+		 */
+		public static function response($message, $data, $code = 200) {
+			return ['success' => true, 'data' => $data, 'code' => $code, 'message' => $message];
+		}
+
+		/**
+		 * @param       $message
+		 * @param array $data
+		 * @param int   $code
+		 *
+		 * @return array
+		 */
+		public static function error($message, array $data = [], $code = 422) {
+			return ['success' => false, 'data' => $data, 'code' => $code, 'message' => $message];
+		}
+
+		/**
+		 * @param     $message
+		 * @param int $code
+		 *
+		 * @return array
+		 */
+		public static function success($message, $code = 200) {
+			return ['success' => true, 'message' => $message, 'code' => $code];
+		}
+	}
