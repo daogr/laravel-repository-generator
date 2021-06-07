1b90d84bbe2c07646fd52c3a70dd242ec666ff90
daogr <dao@ddais.com>
2021-06-07 17:18:43 +0300

    Initial commit

---
+++ src/Exceptions/RepositoryException.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace Otodev\Exceptions;
+
+    use Exception;
+
+    /**
+     * Class RepositoryException
+     * @package Otodev\Exceptions
+     */
+    class RepositoryException extends Exception {
+    }
