69d2324c9ad0fcad2313b55728abaa469ccf33a7
daogr <dao@ddais.com>
2021-06-10 23:33:01 +0300

    Initial commit

---
+++ src/Console/Commands/ApiModelMakeCommand.php
@@ -0,0 +1,74 @@
+<?php
+
+	namespace Otodev\Console\Commands;
+
+	use Illuminate\Console\GeneratorCommand;
+	use Illuminate\Contracts\Filesystem\FileNotFoundException;
+	use Illuminate\Support\Str;
+	use Symfony\Component\Console\Input\InputArgument;
+
+	class ApiModelMakeCommand extends GeneratorCommand {
+		/**
+		 * The name and signature of the console command.
+		 *
+		 * @var string
+		 */
+		protected $name = 'make:model:api';
+		/**
+		 * The console command description.
+		 *
+		 * @var string
+		 */
+		protected $description = 'Create a new api model class';
+
+		/**
+		 * Get the stub file for the generator.
+		 *
+		 * @return string
+		 */
+		protected function getStub() {
+			return __DIR__ . '/stubs/api_model.stub';
+		}
+
+		/**
+		 * Build the class with the given name.
+		 *
+		 * @param string $name
+		 *
+		 * @return string
+		 *
+		 * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+		 */
+		protected function buildClass($name) {
+			$stub = $this->files->get($this->getStub());
+
+			return $this->replaceNamespace($stub, $name)->replaceTableName($stub, $name)->replaceClass($stub, $name);
+		}
+
+		/**
+		 * Replace the class name for the given stub.
+		 *
+		 * @param string $stub
+		 * @param string $name
+		 *
+		 * @return $this
+		 */
+		protected function replaceTableName(&$stub, $name) {
+			$stub = str_replace(['DummyTable', '{{ dummy_table }}', '{{dummy_table}}'], Str::afterLast(Str::lower($name), '\\'), $stub);
+
+			return $this;
+		}
+
+		/**
+		 * Get the default namespace for the class.
+		 *
+		 * @param string $rootNamespace
+		 *
+		 * @return string
+		 */
+		protected function getDefaultNamespace($rootNamespace) {
+			return $rootNamespace . '\Models';
+		}
+
+
+	}
