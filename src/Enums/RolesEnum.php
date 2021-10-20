24dbd28a8ba9637577da89d70b5fd0862fdf93a8
daogr <dao@ddais.com>
2021-10-20 23:22:25 +0300

    Initial commit

---
+++ src/Enums/RolesEnum.php
@@ -0,0 +1,47 @@
+<?php
+	
+	namespace Otodev\Enums;
+	
+	use Illuminate\Support\Facades\Auth;
+	
+	class RolesEnum {
+		
+		/**
+		 * User Roles
+		 */
+		const SUPER_ADMINS = 1;
+		const ADMINS       = 10;
+		
+		/**
+		 * Determine if the user has super admin privileges.
+		 *
+		 * @param null $user
+		 *
+		 * @return bool
+		 */
+		public static function isSuperAdmin($user = null) {
+			if(!$user) {
+				$user = Auth::user();
+			}
+			
+			return ($user->company_id == static::SUPER_ADMINS)
+				&& ($user->role->company_id == static::SUPER_ADMINS)
+				&& $user->role->admin;
+		}
+		
+		/**
+		 * Determine if the user has admin privileges.
+		 *
+		 * @param null $user
+		 *
+		 * @return bool
+		 */
+		public static function isAdmin($user = null) {
+			if(!$user) {
+				$user = Auth::user();
+			}
+			
+			return $user->role->admin;
+		}
+		
+	}
---
+++ src/Traits/CastAttributeValue.php
@@ -0,0 +1,66 @@
+<?php
+	
+	namespace Otodev\Traits;
+	
+	use Illuminate\Support\Collection as BaseCollection;
+	
+	trait CastAttributeValue {
+		
+		/**
+		 * Cast an attribute to a native PHP type.
+		 *
+		 * @param string $key
+		 * @param mixed  $value
+		 *
+		 * @return mixed
+		 */
+		public function castAttributeValue($key, $value) {
+			if(is_null($value)) {
+				return $value;
+			}
+			
+			switch($key) {
+				case 'int':
+				case 'integer':
+					return (int) $value;
+				case 'real':
+				case 'float':
+				case 'double':
+					return $this->fromFloat($value);
+				case 'decimal':
+					return $this->asDecimal($value, explode(':', $this->getCasts()[$key], 2)[1]);
+				case 'string':
+					return (string) $value;
+				case 'bool':
+				case 'boolean':
+					return (bool) $value;
+				case 'object':
+					return $this->fromJson($value, true);
+				case 'array':
+				case 'json':
+					return $this->fromJson($value);
+				case 'collection':
+					return new BaseCollection($this->fromJson($value));
+				case 'date':
+					return $this->asDate($value);
+				case 'datetime':
+				case 'custom_datetime':
+					return $this->asDateTime($value);
+				case 'timestamp':
+					return $this->asTimestamp($value);
+			}
+			
+			return $value;
+		}
+		
+		/**
+		 * Cast all values to the appropriate type
+		 *
+		 * @param $value
+		 *
+		 * @return mixed
+		 */
+		public function getValueAttribute($value) {
+			return $this->castAttributeValue($this->getRawOriginal('cast'), $value);
+		}
+	}
