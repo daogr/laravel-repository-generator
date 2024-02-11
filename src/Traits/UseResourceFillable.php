c3a57fe2507559bb228e1ab491225ff5aa307441
Dimitris Ntais <dao@ddais.com>
2024-02-11 20:04:56 +0200

    Initial commit

---
+++ src/Traits/UseResourceFillable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Otodev\Traits;
+
+use Illuminate\Support\Str;
+
+trait UseResourceFillable
+{
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array
+     */
+    public function transformed()
+    {
+        return $this->fillable();
+    }
+
+    /**
+     * Transform with fillable array.
+     *
+     * @return array
+     */
+    protected function fillable()
+    {
+        $fillable = $this->getFillable() ?? $this->additional['fillable'] ?? [];
+
+        return collect($fillable)->mapWithKeys(function ($key) {
+            if (isset($this->{$key})) {
+                if (is_skipped_resource()) {
+                    return [$key => $this->{$key}];
+                } else {
+                    return [Str::studly($key) => $this->{$key}];
+                }
+            }
+
+            return [];
+        })->toArray();
+    }
+}
