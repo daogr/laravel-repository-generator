df853083a0fd8ad2e3c90e64a4c95ce54a7e44d8
Dimitris Ntais <dao@ddais.com>
2024-02-15 09:51:49 +0200

    Initial commit

---
+++ src/Traits/HasRequestCriteriaFilter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Otodev\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+
+trait HasRequestCriteriaFilter
+{
+
+    /**
+     * @param Builder|Model $model
+     * @param               $filter
+     *
+     * @return Builder|Model
+     */
+    protected function applyFilter($model, $filter)
+    {
+        if (!empty($filter)) {
+
+            if (Arr::accessible($filter)) {
+                $filter = collect($this->fixRequestMapKeys($filter))->intersectByKeys(array_fill_keys($model->getModel()->getFillable(), ''))->all();
+
+                foreach ($filter as $key => $value) {
+                    $model = $model->where($key, $value);
+                }
+
+            }
+        }
+
+        return $model;
+    }
+}
